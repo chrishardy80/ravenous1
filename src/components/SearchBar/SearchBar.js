599e3da611327e906daf6c5e91d91bfec6b8ac3f
Chris Hardy <chrishardy@Chriss-MacBook-Pro.local>
2017-10-15 12:44:56 +0100

    first commit

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import './SearchBar.css';
+
+let sortByOptions = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count',
+};
+
+
+
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+
+    };
+  this.handleTermChange = this.handleTermChange.bind(this);
+  this.handleLocationChange = this.handleLocationChange.bind(this);
+  this.handleSearch = this.handleSearch.bind(this);
+
+  }
+  renderSortByOptions() {
+    return Object.keys(sortByOptions).map(sortByOption => {
+      let sortByOptionsValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionsValue} className={this.getSortByClass(sortByOptionsValue)} onClick={this.handleSortByChange.bind(this, sortByOptionsValue)}>{sortByOption}</li>;
+    });
+  }
+  getSortByClass(sortByOption) {
+    if (this.state.sortBy === sortByOption)
+    {return 'active';}
+    else {
+      return '';
+    }
+  };
+  handleSortByChange(sortByOption) {
+    this.setState({ sortBy: sortByOption });
+  }
+  handleTermChange(event) {
+    this.setState({
+      term: event.target.value
+    });
+  }
+  handleLocationChange(event) {
+    this.setState({
+      location: event.target.value
+    });
+  }
+  handleSearch(event) {
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div className="SearchBar">
+  <div className="SearchBar-sort-options">
+    <ul>
+      {this.renderSortByOptions()}
+    </ul>
+  </div>
+  <div className="SearchBar-fields">
+    <input placeholder="Search Businesses" onChange={this.handleTermChange} onChange={this.handleLocationChange}  />
+    <input placeholder="Where?" onChange={this.handleTermChange} onChange={this.handleLocationChange} />
+  </div>
+  <div className="SearchBar-submit" onClick={this.handleSearch}>
+    <a>Lets Go</a>
+  </div>
+</div>
+    );
+
+  }
+}
+
+
+export default SearchBar;
